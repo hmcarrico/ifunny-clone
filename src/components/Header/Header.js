03dfa57ddb26ad16bcd8ce5cccfd6e91d360213b
Hunter Carrico <huntercarrico@Hunters-Air.frontierlocal.net>
2019-01-02 08:42:56 -0800

    header mobile styled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MenuIcon from '../../media/white-burger-icon.png';
 import './Header.css';
 
 class Header extends Component {
@@ -7,11 +8,12 @@ class Header extends Component {
             <div className='header-main-back'>
                 <div className='header-max-width'>
                     <div className='header-title-logo'>
+                        <img src={MenuIcon} className='menu-hamburger-icon'/>
                         <img src={'https://vignette.wikia.nocookie.net/ifunny-app/images/c/c9/Logo.png/revision/latest?cb=20160107135758'} alt='logo'/>
                         <div>iFunny</div>
+                        <img src={'https://images.vexels.com/media/users/3/132068/isolated/preview/f9bb81e576c1a361c61a8c08945b2c48-search-icon-by-vexels.png'} alt='search icon' className='header-search-icon'/>
                     </div>
                     <div className='header-search-bar'>
-                        {/* <div><img src={'https://images.vexels.com/media/users/3/132068/isolated/preview/f9bb81e576c1a361c61a8c08945b2c48-search-icon-by-vexels.png'} alt='search icon'/></div> */}
                         <div><input placeholder='search'/></div>
                     </div>
                     <div className='header-links'>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
 import Header from '../Header/Header';
+import './Home.css';
 
 class Home extends Component{
     render(){
         return (
             <div>
                 <Header />
-                THIS IS THE HOME PAGE
+                <div className='main-page-parent-flex'>
+                    <div>
+                        <p style={{color: 'rgb(255,207,0)', marginTop: 0}}>featured</p>
+                        <p>Animals & Nature</p>
+                        <p>Anime & Manga</p>
+                        <p>Art & Creative</p>
+                        <p>Cars</p>
+                        <p>Celebrities</p>
+                        <p>Gaming</p>
+                        <p>Girls</p>
+                        <p>Internet</p>
+                        <p>Memes</p>
+                        <p>Movies</p>
+                        <p>Other</p>
+                        <p>Politics</p>
+                        <p>Science & Tech</p>
+                        <p>Sports</p>
+                        <p>TV shows</p>
+                    </div>
+                    <div>
+                        <img src={'https://img.ifcdn.com/images/ace0f6f8edc0635194886587037d7f0b893b374fce63aa8826e6c3b309f36607_1.jpg'} />
+                    </div>
+                    <div>
+                        <div>TOUCH TO UNLOCK FUN</div>
+                        <div>TRENDING TAGS</div>
+                        <div>FOLLOW iFunny</div>
+                    </div>
+                </div>
             </div>
         )
     }
