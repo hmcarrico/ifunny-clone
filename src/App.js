30b236020063a2c7a4c8ec1dc14fcae2406afbfa
Hunter Carrico <huntercarrico@Hunters-Air.frontierlocal.net>
2019-01-02 16:41:03 -0800

    home page details added and about component started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import routes from './routes';
+import Header from './components/Header/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='temp-app'>
+        <Header />
         {routes}
       </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import Header from '../Header/Header';
+import iceCream from '../../media/ifunny-ice-cream.png';
 import './Home.css';
 
 class Home extends Component{
+    constructor(){
+        super();
+        this.state = {
+            trendingTags: ['spicy', 'comic', 'animals', 'birdbox', 'games', 'resolutions2019', 'chungus', 'smoothie', 'video', 'ifunnyclones', 'ifunnycalifornia', 'tagtuesday', 'wholesomewednesday', 'firstfeat', 'smolboi', 'memerewind2018']
+        }
+    }
+
     render(){
+        let trending = this.state.trendingTags.map(tag => {
+            return <div>
+                <button className='main-page-trending-tag-button'>#{tag}</button>
+            </div>
+        })
         return (
             <div>
-                <Header />
                 <div className='main-page-parent-flex'>
                     <div className='main-page-categories'>
                         <p style={{color: 'rgb(255,207,0)', marginTop: 0}}>featured</p>
@@ -28,12 +40,25 @@ class Home extends Component{
                     </div>
                     <div className='main-page-meme-holder'>
                         <p className='main-page-mobile-countdown'>the next fun train arrives in XXXX</p>
-                        <img src={'https://img.ifcdn.com/images/ace0f6f8edc0635194886587037d7f0b893b374fce63aa8826e6c3b309f36607_1.jpg'} className='placeholder-meme'/>
+                        <img src={'https://img.ifcdn.com/images/4d184f5d1fad0f0ea79a95487c558ff6dff10ca56e4149ddb4ec3ea56c34ee62_1.jpg'} className='placeholder-meme'/>
                     </div>
                     <div className='main-page-right-side-bar'>
-                        <div>TOUCH TO UNLOCK FUN</div>
-                        <div>TRENDING TAGS</div>
-                        <div>FOLLOW iFunny</div>
+                        <div>
+                            <img src={iceCream} />
+                            <div className='home-page-app-links'>
+                                <div>App Store</div>
+                                <div>Google Play</div>
+                                <div>Amazon</div>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='main-page-trending-title'>trending tags</p>
+                            <div className='main-page-trending-tag'>
+                                {trending}
+                            </div>
+                        </div>
+                        <hr />
+                        <div>follow iFunny</div>
                     </div>
                 </div>
             </div>
